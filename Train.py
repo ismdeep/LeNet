897cb9789148dec69761f41809db39122127c67a
L. Jiang <l.jiang.1024@gmail.com>
2021-05-18 10:33:01 +0800

    chore: add requirements.txt

--- Train.py
+++ Train.py
@@ -1,9 +1,7 @@
-
 import tensorflow.examples.tutorials.mnist.input_data as input_data
 import tensorflow as tf
 import config as cfg
 import os
-import lenet
 from lenet import Lenet
 
 
@@ -15,7 +13,6 @@ def main():
     lenet = Lenet()
     max_iter = cfg.MAX_ITER
 
-
     saver = tf.train.Saver()
     if os.path.exists(parameter_path):
         saver.restore(parameter_path)
@@ -25,14 +22,13 @@ def main():
     for i in range(max_iter):
         batch = mnist.train.next_batch(50)
         if i % 100 == 0:
-            train_accuracy = sess.run(lenet.train_accuracy,feed_dict={
-                lenet.raw_input_image: batch[0],lenet.raw_input_label: batch[1]
+            train_accuracy = sess.run(lenet.train_accuracy, feed_dict={
+                lenet.raw_input_image: batch[0], lenet.raw_input_label: batch[1]
             })
             print("step %d, training accuracy %g" % (i, train_accuracy))
-        sess.run(lenet.train_op,feed_dict={lenet.raw_input_image: batch[0],lenet.raw_input_label: batch[1]})
+        sess.run(lenet.train_op, feed_dict={lenet.raw_input_image: batch[0], lenet.raw_input_label: batch[1]})
     save_path = saver.save(sess, parameter_path)
+
 
 if __name__ == '__main__':
     main()
-
-
--- UI.py
+++ UI.py
@@ -1,20 +1,20 @@
 import tkinter
-from PIL import Image,ImageDraw
+from PIL import Image, ImageDraw
 from Inference import inference
 
+
 class MyCanvas:
-    def __init__(self,root):
-        self.root=root
-        self.canvas=tkinter.Canvas(root,width=256,height=256,bg='black')
+    def __init__(self, root):
+        self.root = root
+        self.canvas = tkinter.Canvas(root, width=256, height=256, bg='black')
         self.canvas.pack()
         self.image1 = Image.new("RGB", (256, 256), "black")
         self.draw = ImageDraw.Draw(self.image1)
-        self.canvas.bind('<B1-Motion>',self.Draw)
+        self.canvas.bind('<B1-Motion>', self.Draw)
 
-
-    def Draw(self,event):
-        self.canvas.create_oval(event.x,event.y,event.x,event.y,outline="white",width = 20)
-        self.draw.ellipse((event.x-10,event.y-10,event.x+10,event.y+10),fill=(255,255,255))
+    def Draw(self, event):
+        self.canvas.create_oval(event.x, event.y, event.x, event.y, outline="white", width=20)
+        self.draw.ellipse((event.x - 10, event.y - 10, event.x + 10, event.y + 10), fill=(255, 255, 255))
 
 
 def main():
@@ -48,6 +48,7 @@ def main():
     label2["font"] = ("Arial, 48")
     label2.pack()
     root.mainloop()
+
 
 if __name__ == '__main__':
     main()
